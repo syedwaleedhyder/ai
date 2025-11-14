b9cec5eb87209de944b5037fb3c4c6e8f680298b
syedwaleedhyder <syedwaleedhyder@gmail.com>
2025-11-14 11:37:05 -0600

    Add first simple agent using smolagents

---
+++ agents/hf_agents_course/unit1-smolagents-first-agent/app.py
@@ -0,0 +1,35 @@
+from smolagents import CodeAgent, HfApiModel, load_tool
+import yaml
+from tools.final_answer import FinalAnswerTool
+
+from Gradio_UI import GradioUI
+
+
+final_answer = FinalAnswerTool()
+model = HfApiModel(
+    max_tokens=2096,
+    temperature=0.5,
+    model_id='Qwen/Qwen2.5-Coder-32B-Instruct',
+    custom_role_conversions=None,
+)
+
+# Import tool from Hub
+image_generation_tool = load_tool("agents-course/text-to-image", trust_remote_code=True)
+
+# Load system prompt from prompt.yaml file
+with open("prompts.yaml", 'r') as stream:
+    prompt_templates = yaml.safe_load(stream)
+    
+agent = CodeAgent(
+    model=model,
+    tools=[final_answer, image_generation_tool],
+    max_steps=6,
+    verbosity_level=1,
+    grammar=None,
+    planning_interval=None,
+    name=None,
+    description=None,
+    prompt_templates=prompt_templates
+)
+
+GradioUI(agent).launch()

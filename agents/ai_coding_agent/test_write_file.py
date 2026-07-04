12c3405f68526eacfd0b8777c885992b31c9c8cc
syedwaleedhyder <syedwaleedhyder@gmail.com>
2026-07-04 15:42:43 -0500

    Add write_file with directory-scoped path validation

---
+++ agents/ai_coding_agent/test_write_file.py
@@ -0,0 +1,15 @@
+from functions.write_file import write_file
+
+
+def main():
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print()
+
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print()
+
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+
+
+if __name__ == "__main__":
+    main()
